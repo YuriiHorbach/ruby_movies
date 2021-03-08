bc8207e829beed3d6ab51e1907c33cae48aedfda
user <gyrii84@gmail.com>
2021-03-08 23:58:25 +0200

    add create, delete functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,14 +13,31 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path
+  end
+
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :rating, :total_gross, :premiere, :description)
     @movie.update(movie_params)
     redirect_to movie_path
   end
 
-
-
-
+  def destroy
+    @movie = Movie.find_by(params[:id])
+    @movie.destroy 
+    redirect_to movies_path
+  end
 end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :total_gross, :premiere, :description)
+  end
