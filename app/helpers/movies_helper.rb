24c50c7f26e72e04a111121c26c863ceb3a56f5b
user <gyrii84@gmail.com>
2021-03-17 15:15:54 +0200

    add new design

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -9,9 +9,9 @@ module MoviesHelper
 
   def image_for(movie)
     if movie.image_file_name.blank?
-      image_tag 'moviemarathon.png'
+      image_tag('placeholder.png')
     else
-      image_tag movie.image_file_name
-    end 
+      image_tag(movie.image_file_name)
+    end
   end
 end
--- db/migrate/20210213191944_add_column_to_movie.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToMovie < ActiveRecord::Migration[6.1]
-  def change
-    add_column :movies, :premiere, :date
-  end
-end
